1a1af2b6e7f5d930edd3c2f4685e61faddbeb8ac
Josef Pospíšil <josef.pospisil@laststar.eu>
2017-03-21 12:43:10 +0100

    Fix text on setting current slide, refactor tests

--- test/cljs/showrum/events_test.cljs
+++ test/cljs/showrum/events_test.cljs
@@ -11,23 +11,23 @@
   (let [init-state {:slide/current 2 :deck/slides-count 3}]
     (testing "Set Current Slide"
       (let [ev (sut/->SetCurrentSlide 1)]
-        #(is (= {:slide/current 1 :deck/slides-count 2}
-                (ptk/update % init-state)))))
+        (is (= (ptk/update ev init-state)
+               {:slide/current 1 :deck/slides-count 3}))))
     (testing "Navigate Next Slide"
       (let [ev (sut/->NavigateNextSlide)]
-        (rxt/on-value (ptk/watch ev init-state nil)
-                      #(is (= (sut/->SetCurrentSlide 3) %)))))
+        (rxt/on-value (ptk/watch ev init-state (rxt/empty))
+                      (fn [event] (is (= event (sut/->SetCurrentSlide 3)))))))
     (testing "Navigate Previous Slide"
       (let [ev (sut/->NavigatePreviousSlide)]
-        (rxt/on-value (ptk/watch ev init-state nil)
-                      #(is (= (sut/->SetCurrentSlide 1) %)))))))
+        (rxt/on-value (ptk/watch ev init-state (rxt/empty))
+                      (fn [event] (is (= event (sut/->SetCurrentSlide 1)))))))))
 
 (deftest test-searching []
   (testing "Toggle Search Panel"
     (let [ev (sut/->ToggleSearchPanel)]
-      (is (= {:search/active true}
-             (ptk/update ev {:search/active false})))))
+      (is (= (ptk/update ev {:search/active false})
+             {:search/active true}))))
   (testing "Set Active Search Result"
     (let [ev (sut/->SetActiveSearchResult 1)]
-      (is (= {:search/results-count 2 :search/result 1}
-             (ptk/update ev {:search/results-count 2}))))))
+      (is (= (ptk/update ev {:search/results-count 2})
+             {:search/results-count 2 :search/result 1})))))
